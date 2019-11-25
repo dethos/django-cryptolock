f1ed8f3827ab089a03a7a188e53c89ed1c08bd52
Gonçalo Valério <gon@ovalerio.net>
2019-11-25 19:24:46 +0000

    add missing f-string symbol

--- django_cryptolock/utils.py
+++ django_cryptolock/utils.py
@@ -11,7 +11,7 @@ def verify_signature(address: str, challenge: str, signature: str) -> bool:
     host = settings.MONERO_WALLET_RPC_HOST
     user = settings.MONERO_WALLET_RPC_USER
     pwd = settings.MONERO_WALLET_RPC_PASS
-    wallet_rpc = AuthServiceProxy("{protocol}://{user}:{pwd}@{host}/json_rpc")
+    wallet_rpc = AuthServiceProxy(f"{protocol}://{user}:{pwd}@{host}/json_rpc")
 
     result = wallet_rpc.verify(
         {"data": challenge, "address": address, "signature": signature}
