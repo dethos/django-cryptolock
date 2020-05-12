c4daaba4f1c4312827524f74a3199aaaf925db21
Gonçalo Valério <gon@ovalerio.net>
2020-05-12 17:02:10 +0100

    format migration file

--- django_cryptolock/migrations/0003_challenge.py
+++ django_cryptolock/migrations/0003_challenge.py
@@ -7,23 +7,40 @@ import model_utils.fields
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('django_cryptolock', '0002_auto_20200218_1312'),
-    ]
+    dependencies = [("django_cryptolock", "0002_auto_20200218_1312")]
 
     operations = [
         migrations.CreateModel(
-            name='Challenge',
+            name="Challenge",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
-                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
-                ('challenge', models.CharField(max_length=150)),
-                ('expires', models.DateTimeField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "created",
+                    model_utils.fields.AutoCreatedField(
+                        default=django.utils.timezone.now,
+                        editable=False,
+                        verbose_name="created",
+                    ),
+                ),
+                (
+                    "modified",
+                    model_utils.fields.AutoLastModifiedField(
+                        default=django.utils.timezone.now,
+                        editable=False,
+                        verbose_name="modified",
+                    ),
+                ),
+                ("challenge", models.CharField(max_length=150)),
+                ("expires", models.DateTimeField()),
             ],
-            options={
-                'verbose_name': 'Challenge',
-                'verbose_name_plural': 'Challenges',
-            },
-        ),
+            options={"verbose_name": "Challenge", "verbose_name_plural": "Challenges"},
+        )
     ]
